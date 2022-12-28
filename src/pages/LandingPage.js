ac7b36e3a08ae36b4915fcf18f03e2443a87eea7
saurabhsiwa <saurabhsiwa2018@gmail.com>
2022-12-28 21:09:59 +0530

    animation

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import { ReactComponent as Svg1 } from '../assets/vectors/codinganimate.svg';
+import { ReactComponent as Svg2 } from '../assets/vectors/software-engineer.svg';
+import 'animate.css';
+
 const LandingPage = (props) => {
-  return <>Landing page</>;
+  const [alternate, setAlternate] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (alternate) {
+        setAlternate(false);
+      } else {
+        setAlternate(true);
+      }
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, [alternate]);
+
+  return (
+    <Container>
+      <Grid columns={2} padded="vertically">
+        <Grid.Column></Grid.Column>
+        <Grid.Column>
+          {alternate ? (
+            <div className="animate__animated animate__fadeIn">
+              <Svg1 />
+            </div>
+          ) : (
+            <div className="animate__animated animate__fadeInRight">
+              <Svg2 />
+            </div>
+          )}
+        </Grid.Column>
+      </Grid>
+    </Container>
+  );
 };
 
 export default LandingPage;
