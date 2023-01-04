a5d1342e7b374311f478e485b6fe827e6fa3fc4a
saurabhsiwa <saurabhsiwa2018@gmail.com>
2023-01-04 17:39:37 +0530

    cards and ecommerceshowpage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,8 +3,10 @@ import { Container } from 'semantic-ui-react';
 import { Grid } from 'semantic-ui-react';
 import { ReactComponent as Svg1 } from '../assets/vectors/codinganimate.svg';
 import { ReactComponent as Svg2 } from '../assets/vectors/software-engineer.svg';
+import TechCardContainer from '../containers/TechCardContainer';
 import './LandingPage.scss';
 import 'animate.css';
+import EcommerceShowCase from '../containers/EcommerceShowCase';
 
 const LandingPage = (props) => {
   const [alternate, setAlternate] = useState(false);
@@ -22,22 +24,34 @@ const LandingPage = (props) => {
   }, [alternate]);
 
   return (
-    <Container>
-      <Grid columns={2} padded="vertically" className="landingContainer">
-        <Grid.Column width={9}></Grid.Column>
-        <Grid.Column width={7} className="vectorContainer">
-          {alternate ? (
-            <div className="animate__animated animate__fadeIn">
-              <Svg1 />
-            </div>
-          ) : (
-            <div className="animate__animated animate__fadeInRight">
-              <Svg2 />
-            </div>
-          )}
-        </Grid.Column>
-      </Grid>
-    </Container>
+    <>
+      <Container>
+        <Grid columns={2} padded="vertically" className="landingContainer">
+          <Grid.Column width={9}></Grid.Column>
+          <Grid.Column width={7} className="vectorContainer">
+            {alternate ? (
+              <div className="animate__animated animate__fadeIn">
+                <Svg1 />
+              </div>
+            ) : (
+              <div className="animate__animated animate__fadeInRight">
+                <Svg2 />
+              </div>
+            )}
+          </Grid.Column>
+        </Grid>
+      </Container>
+      <Container className="technology" fluid>
+        <Container>
+          <TechCardContainer />
+        </Container>
+      </Container>
+      <Container className="ecommerceShowCase" fluid>
+        <Container>
+          <EcommerceShowCase />
+        </Container>
+      </Container>
+    </>
   );
 };
 
